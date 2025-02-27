439bf58279dd84259e13d1ec67b51732f9d5c27b
rezatd-develop <reza.td.develop@gmail.com>
2025-02-27 05:04:24 -0800

    set basics of profile routes

--- src/app/[lang]/cart/page.js
+++ src/app/[lang]/cart/page.js
@@ -1,16 +1,16 @@
 import { getDictionary } from '../../[lang]/dictionaries';
-import ProfilePage from '../../../view/public/profile/ProfilePage';
+import CartPage from '../../../view/public/cart/CartPage';
 import ProfileLayout from '../../../layouts/profile/ProfileLayout';
 import '../../../styles/common.css';
 import App from './../App';
 
-export default async function Profiler({ params }) {
+export default async function Cart({ params }) {
     const t = await getDictionary(params?.lang);
 
     return (
         <App params={params} t={t} lang={params?.lang} >
             <ProfileLayout>
-                <ProfilePage />
+                <CartPage />
             </ProfileLayout>
         </App >
     );
