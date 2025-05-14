38bc511875f81a655c5f31bda33c7ec4e4b5712e
rezatd-develop <reza.td.develop@gmail.com>
2025-05-14 11:28:20 +0330

    unsafe error

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body>{children}</body>
     </html>
   );
